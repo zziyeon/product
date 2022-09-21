052d80d6d9a8ecb38a32b26ebd244f2a3e297657
zziyeon <jiyunsara99@naver.com>
2022-09-22 00:42:30 +0900

    수정

--- product3/src/main/java/com/kh/demo/web/AttachFileController.java
+++ product3/src/main/java/com/kh/demo/web/AttachFileController.java
@@ -1,28 +1,36 @@
 package com.kh.demo.web;
 
+import com.kh.demo.domain.common.file.UploadFile;
+import com.kh.demo.domain.common.file.UploadFileDAO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriUtils;
 
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Slf4j
 @Controller
-
+@RequiredArgsConstructor
 @RequestMapping("/attach")
 public class AttachFileController {
   @Value("${attach.root_dir}")
   private String attachRoot;    //첨부파일 루트경로
-
-
+  private final UploadFileDAO uploadFileDAO;
 
   //이미지
-  @ResponseStatus(HttpStatus.OK)
+//  @ResponseStatus(HttpStatus.OK)
   @ResponseBody
   @GetMapping("/img/{attachCode}/{storeFileName}")
   public Resource img(
@@ -38,12 +46,22 @@ public class AttachFileController {
   @GetMapping("/file/{attachCode}/{fid}")
   public ResponseEntity<Resource> file(
           @PathVariable String attachCode,
-          @PathVariable Long fid ) {
+          @PathVariable Long fid )throws MalformedURLException {
     ResponseEntity<Resource> res = null;
 
-    O
+    Optional<UploadFile> uploadFile = uploadFileDAO.findFileByUploadFileId(fid);
+    if(uploadFile.isEmpty()) return res;
 
-    uploadFileDAO.findFileByUploadFileId(fid);
+    UploadFile attachFile = uploadFile.get();
+    String storeFileName = attachFile.getStoreFileName();
+    Resource resource = new UrlResource("file:///"+attachRoot+"/"+attachCode+"/"+storeFileName);
+    String encode = UriUtils.encode(storeFileName, StandardCharsets.UTF_8);
+
+    String contentDisposition = "attachmemt; filename=\""+encode+"\"";
+
+    res = ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION,contentDisposition)
+            .body(resource);
     return res;
   }
 
