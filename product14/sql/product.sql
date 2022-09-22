75f3f4599bb51c0e08e8ec8cc0485fc25a594fa4
zziyeon <jiyunsara99@naver.com>
2022-09-22 17:32:38 +0900

    통합 구현_시험

--- product14/sql/product.sql
+++ product14/sql/product.sql
@@ -1,8 +1,18 @@
 drop table product;
 
 create table product(
-    pid NUMBER(4),
+    pid NUMBER(10),
     pname varchar2(60),
-    count number(2),
-    price number(6)
+    count number(10),
+    price number(10)
 );
+
+--기본키
+alter table product add constraint product_product_id_pk primary key(product_id);
+
+--시퀀스생성
+drop sequence product_pid_seq;
+create sequence product_pid_seq;
+
+
+commit;
