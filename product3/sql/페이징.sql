567e60906f59376d21d88c88d88240a697f17891
zziyeon <jiyunsara99@naver.com>
2022-09-26 19:08:31 +0900

    페이징 sql 추가

--- product3/sql/페이징.sql
+++ product3/sql/페이징.sql
@@ -11,12 +11,15 @@ select *
 from ( select rownum no, product_id, pname, quantity, price 
         from product) t1
 where  t1.no between 1 and 10;
+
 -- case2)
 select * 
 from ( select row_number() over(order by product_id desc) no,
             product_id, pname, quantity, price
         from product) t1
-        where t1.no between 1 and 10;
+        where t1.no between 1 and 10
+            and ;
+        
        
 -- 프로시저 생성
 create or replace procedure insert_product
